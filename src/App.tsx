70663bf84a9705d889b761da46ffea708a2f8d79
Ahmed Hamdy <hamdyjs@gmail.com>
2019-08-26 10:44:45 +0200

    Remove useless menu items

--- src/App.tsx
+++ src/App.tsx
@@ -30,22 +30,16 @@ const App: React.FC = () => {
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="index.html">Home <span className="sr-only">(current)</span></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="index.html">Link</a>
-                            </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="index.html" id="navbarDropdown" role="button"
                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Dropdown
+                                    CTE Web Interface
                                 </a>
                                 <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a className="dropdown-item" href="index.html">Action</a>
-                                    <a className="dropdown-item" href="index.html">Another action</a>
-                                    <div className="dropdown-divider"></div>
-                                    <a className="dropdown-item" href="index.html">Something else here</a>
+                                    <a className="dropdown-item" href="/tracer">Signal Tracer</a>
+                                    {/*<a className="dropdown-item" href="index.html">Another action</a>*/}
+                                    {/*<div className="dropdown-divider"></div>*/}
+                                    {/*<a className="dropdown-item" href="index.html">Something else here</a>*/}
                                 </div>
                             </li>
                         </ul>
