c567d78c59fc9bd488b487a620d9906d1abf80a6
Ahmed Hamdy <hamdyjs@gmail.com>
2019-08-26 15:32:09 +0200

    Add loading spinner to signals'

--- src/components/Tracer.tsx
+++ src/components/Tracer.tsx
@@ -1,18 +1,21 @@
 import React, {useState, useEffect, ChangeEvent} from "react";
 
 const Tracer: React.FC = () => {
+    let [loading, setLoading] = useState(true);
     let [signals, setSignals] = useState(([] as string[]));
     let [activeSignals, setActiveSignals] = useState([] as string[]);
 
     useEffect(() => {
         // TODO: Integrate with the CTE server
-        setTimeout(() => setSignals(["Signal 1", "Signal 2", "Signal 3", "Signal 4"]), 500);
+        setTimeout(() => setSignals(["Signal 1", "Signal 2", "Signal 3", "Signal 4"]), 2000);
+        setTimeout(() => setLoading(false), 2000);
     }, []);
 
     function onSignalCheckedChange({target: {value, checked}}: ChangeEvent<HTMLInputElement>) {
         let newActiveSignals = [...activeSignals];
         if (checked && !newActiveSignals.includes(value)) newActiveSignals.push(value);
-        else if (!checked && newActiveSignals.includes(value)) newActiveSignals = newActiveSignals.filter(s => s !== value);
+        else if (!checked && newActiveSignals.includes(value))
+            newActiveSignals = newActiveSignals.filter(s => s !== value);
 
         setActiveSignals(newActiveSignals);
     }
@@ -22,17 +25,25 @@ const Tracer: React.FC = () => {
             <div className="bg-light border-right" id="sidebar-wrapper">
                 <div className="sidebar-heading">Signals</div>
                 <div className="list-group list-group-flush">
-                    {signals.map((signal, i) => {
-                        return (
-                            <div className="form-check ml-3" key={i}>
-                                <input onChange={onSignalCheckedChange} className="form-check-input" type="checkbox"
-                                       name="signal" value={signal} id={"signal"+ i}/>
-                                <label className={"form-check-label " +
-                                    (activeSignals.includes(signal) ? "text-success" : "text-danger")}
-                                    htmlFor={"signal"+ i}>{signal}</label>
+                    {
+                        loading ? (
+                            <div className="spinner-border spinner-border-sm ml-4" role="status">
+                                <span className="sr-only">Loading...</span>
                             </div>
-                        );
-                    })}
+                        ) : (
+                            signals.map((signal, i) => {
+                                return (
+                                    <div className="form-check ml-3" key={i}>
+                                        <input onChange={onSignalCheckedChange} className="form-check-input"
+                                               type="checkbox" name="signal" value={signal} id={"signal"+ i}/>
+                                        <label className={"form-check-label " +
+                                            (activeSignals.includes(signal) ? "text-success" : "text-danger")}
+                                            htmlFor={"signal"+ i}>{signal}</label>
+                                    </div>
+                                );
+                            })
+                        )
+                    }
                 </div>
             </div>
             <div id="page-content-wrapper">
