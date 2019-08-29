dbfc8fb57c4bf8368f836167534f064cceb897a0
Ahmed Hamdy <hamdyjs@gmail.com>
2019-08-29 16:34:46 +0200

    Implement drawing multi charts

--- src/components/MultiCharts.tsx
+++ src/components/MultiCharts.tsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Signal from "../classes/Signal";
+import Chart from "chart.js";
+
+let charts: {[s: string]: Chart} = {};
 
 const MultiCharts: React.FC<{signals: Signal[]}> = (props) => {
     let {signals} = props;
+
+    useEffect(() => {
+        for (let signal of signals) {
+            if (charts[signal.name]) charts[signal.name].destroy();
+            charts[signal.name] = new Chart(signal.name, {
+                type: "line",
+                options: {
+                    maintainAspectRatio: false,
+                },
+                data: {
+                    labels: signal.readings.map((reading) => reading.timestamp.toString()),
+                    datasets: [{
+                        data: signal.readings.map((reading) => reading.value),
+                        fill: false,
+                        borderColor: "rgb(255, 0, 0)",
+                        label: "",
+                    }],
+                },
+            });
+        }
+    }, [signals]);
 
     return (
         <div>
@@ -11,7 +35,7 @@ const MultiCharts: React.FC<{signals: Signal[]}> = (props) => {
                     <div className="card mt-1" key={i}>
                         <div className="card-header"><b>{signal.name}</b></div>
                         <div className="card-body">
-                            <canvas id={signal.name} width="200" height="50" style={{display: "none"}} />
+                            <canvas id={signal.name} width="200" height="300" />
                         </div>
                     </div>
                 ))
