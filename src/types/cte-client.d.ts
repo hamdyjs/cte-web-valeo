de38c685d2c9be36d9038c67ff3bcf8349829b46
Ahmed Hamdy <hamdyjs@gmail.com>
2019-09-05 13:00:18 +0200

    Add type definition for cte-client

---
+++ src/types/cte-client.d.ts
@@ -0,0 +1,20 @@
+
+export declare global {
+    interface Window {
+        client: any;
+        Empty: any;
+    }
+}
+
+export interface Trace {
+	entries: TraceEntry[];
+}
+
+export interface TraceEntry {
+	timestamp: number;
+	name: string;
+	type: string;
+	value: number;
+	unit: string;
+	valueTextMap: map<number, string>;
+}
